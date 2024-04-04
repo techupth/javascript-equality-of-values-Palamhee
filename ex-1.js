86af406a9ec550190e5cb2ff7882ca61fd6e995a
Palamhee <palarat.pj@gmail.com>
2024-04-04 23:48:08 +1300

    fix missing answer

--- ex-1.js
+++ ex-1.js
@@ -3,7 +3,7 @@
 Object.is(10, 20); // false
 Object.is(0, -0); // false
 Object.is(NaN, 100); // false
-Object.is("one", "two"); // 
+Object.is("one", "two"); // false
 Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); // true
 Object.is(null, undefined); // false
 
